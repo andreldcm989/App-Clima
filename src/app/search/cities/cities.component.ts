50f42a1131d9e50bde2cccd80414b26c283151fa
Andre Cruz <andreldcruz@outlook.com>
2022-10-05 12:15:50 -0300

    Criação do módulo search e o componente cities

---
+++ src/app/search/cities/cities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cities',
+  templateUrl: './cities.component.html',
+  styleUrls: ['./cities.component.css']
+})
+export class CitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/search/search.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchRoutingModule } from './search-routing.module';
+import { CitiesComponent } from './cities/cities.component';
+
+
+@NgModule({
+  declarations: [
+    CitiesComponent
+  ],
+  imports: [
+    CommonModule,
+    SearchRoutingModule
+  ]
+})
+export class SearchModule { }
