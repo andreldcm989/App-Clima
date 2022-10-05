c95ba32cc95644bdf18de36e4e33ebfc93de3621
Andre Cruz <andreldcruz@outlook.com>
2022-10-05 11:53:03 -0300

    MUDANÇA DE PROJETO: de smartphones para aplicação de Clima

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrandsModule } from './brands/brands.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrandsModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
