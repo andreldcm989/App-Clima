745046e355863c40938ec26788951bfdad3c0421
Andre Cruz <andreldcruz@outlook.com>
2022-10-07 06:14:59 -0300

    Criação do componente weather

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CitiesComponent } from './cities/cities.component';
 import { SearchRoutingModule } from './search-routing.module';
+import { WeatherComponent } from '../weather/weather.component';
 
 @NgModule({
-  declarations: [CitiesComponent],
+  declarations: [CitiesComponent, WeatherComponent],
   imports: [CommonModule, SearchRoutingModule, FormsModule],
   providers: [],
   exports: [CitiesComponent],
