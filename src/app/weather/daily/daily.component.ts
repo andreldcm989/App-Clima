8661c9c9397f189c8b3659c698534380f41f3eae
Andre Cruz <andreldcruz@outlook.com>
2022-10-27 17:08:26 -0300

    refactor(dailyforecast): desabilitado o checked inicial dos inputs

--- src/app/weather/daily/daily.component.ts
+++ src/app/weather/daily/daily.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { dailyForecast } from '../model/dailyForecast';
 
 @Component({
